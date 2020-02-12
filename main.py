abdd8eb222653deaf84b0080e9af2eef937280c5
StarPandaBeg <50238446+StarPandaBeg@users.noreply.github.com>
2020-02-12 13:43:39 +0300

    v2

--- main.py
+++ main.py
@@ -4,14 +4,11 @@
 import vk_api
 
 import time
-import datetime
 import os
 import random
 
-VK_USER_TOKEN = "8c78567fb18f1c2fbdf8cf42acbcaeb4a4701fffff127ef4f0a9252e7924fc033a736c5fc9365fde2f632"#str(os.environ.get("TOKEN"))
-VK_CONTROL_TOKEN = "7d947a1f282554642fe101ec7c99e85a439b259ed95c31615513daa0122287bb9b4cd44217c61c7c9bd1b"#str(os.environ.get("ACCESS_TOKEN"))
-UP = 2#int(os.environ.get("UP"))
-DOWN = 7#int(os.environ.get("DOWN"))
+VK_USER_TOKEN = str(os.environ.get("TOKEN"))
+VK_CONTROL_TOKEN = str(os.environ.get("ACCESS_TOKEN"))
 
 def login(token):
     vk = vk_api.VkApi(token=token)
@@ -25,53 +22,22 @@ def run(api):
     ac_api = login(VK_CONTROL_TOKEN)
     while (True):
         random_s, random_online = get_random()
-        send_message(ac_api, "Жду " + str(random_s) + " секунд до следующего онлайна.")
+        text = format_time(random_s)
+        send_message(ac_api, text)
         wait_for_seconds(random_s)
         send_message(ac_api, "Захожу в онлайн на " + str(random_online) + " секунд.")
         set_online(api, random_online)
         send_message(ac_api, "Оффлайн")
 
 # ------------
+def format_time(s):
+    now_time = int(time.time())
+    next_time = now_time + s
+    resp = "Следущий онлайн будет в " + time.strftime("%H:%M %d.%m.%Y", time.localtime(next_time))
+    return resp
+
 def get_random():
-    temp_date = datetime.datetime.now()
-    if UP < temp_date.hour: 
-        if temp_date.month in [1,3,5,7,8,10,12]:
-            if temp_date.day == 31:
-                if temp_date.month == 12:
-                    temp_new_date = datetime.datetime(temp_date.year+1,1,1,UP,0,0)
-                else:
-                    temp_new_date = datetime.datetime(temp_date.year,temp_date.month+1,1,UP,0,0)
-            else:
-                temp_new_date = datetime.datetime(temp_date.year,temp_date.month,temp_date.day+1,UP,0,0)
-        elif temp_date.month == 2:
-            if (temp_date.year % 4) == 0:
-                if temp_date.day == 29:
-                    temp_new_date = datetime.datetime(temp_date.year,3,1,UP,0,0)
-                else:
-                    temp_new_date = datetime.datetime(temp_date.year,temp_date.month,temp_date.day+1,UP,0,0)
-            else:
-                if temp_date.day == 28:
-                    temp_new_date = datetime.datetime(temp_date.year,3,1,UP,0,0)
-                else:
-                    temp_new_date = datetime.datetime(temp_date.year,temp_date.month,temp_date.day+1,UP,0,0)
-        else:
-            if temp_date.day == 30:
-                temp_new_date = datetime.datetime(temp_date.year,temp_date.month+1,1,UP,0,0)
-            else:
-                temp_new_date = datetime.datetime(temp_date.year,temp_date.month,temp_date.day+1,UP,0,0)
-    else:
-        temp_new_date = datetime.datetime(temp_date.year,temp_date.month,temp_date.day,UP,0,0)
-
-    if temp_new_date.day - temp_date.day == 0:
-        start = 120
-    else:
-        start = (datetime.datetime(temp_date.year,temp_date.month,temp_date.day,DOWN,0,0) - temp_date).total_seconds()
-        if start < 0:
-            start = 120
-
-    delta = temp_new_date - temp_date
-    total = delta.total_seconds()
-    random_s = random.randrange(int(start), int(total), 30)
+    random_s = 5400 + random.randrange(0, 1800, 60)
     random_online = random.randrange(300, 3600, 300)
     return random_s, random_online
 
